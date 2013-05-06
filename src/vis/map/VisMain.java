c22a8bb2938605f83010720523cbf9372f812d22
Aritra Dasgupta <dasgupta.aritra@gmail.com>
2013-05-06 01:33:25 -0400

    Data Reader

--- src/vis/map/VisMain.java
+++ src/vis/map/VisMain.java
@@ -1,11 +1,32 @@
 package vis.map;
 
+import java.io.IOException;
+
+import vis.map.datamodel.DataReader;
+import vis.map.datamodel.DataSet;
+
 public class VisMain {
 	
 	public static void main(String args[]){
 		
 		
-		System.err.println("hiiiiii Wooohoo");
+		DataReader reader = new DataReader();
+		try {
+			reader.readData();
+		} catch (IOException exception) {
+			// TODO Auto-generated catch-block stub.
+			exception.printStackTrace();
+		}
+		
+		DataSet data = reader.getData();
+		
+		for(int i=0; i<data.getNumDimensions(); i++)
+		{
+			
+			
+			System.err.println("Label " +data.getAxisLabel(i));
+		}
+		
 		
 	}
 
---
+++ src/vis/map/datamodel/DataReader.java
@@ -0,0 +1,46 @@
+package vis.map.datamodel;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+/**
+ * Reads a CSV file and sets up a DataSet Object.
+ *
+ * @author ADG.
+ *         Created May 5, 2013.
+ */
+public class DataReader {
+	
+	DataSet data = null;
+
+	public void readData() throws IOException{
+
+		CSVReader reader = new CSVReader(new FileReader("src/data/cars.csv"));
+		data = new DataSet();
+		String[] label = null;
+		String[] nextLine;
+		int rowNum = 0;
+		while ((nextLine = reader.readNext()) != null) {
+			
+			if(rowNum == 0)
+			label = nextLine;
+			rowNum++;
+			
+			// nextLine[] is an array of values from the line
+		//	System.out.println(nextLine[0] + nextLine[1] + "etc...");
+			data.addStringRecord(nextLine, label);
+		}
+
+
+
+	}
+	
+	public DataSet getData(){
+		return data;
+	}
+
+
+}
